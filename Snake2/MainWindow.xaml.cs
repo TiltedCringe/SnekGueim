3c74250a4bbf9133a0be8b9688c3b4bd761e3f8e
Tilted <a2006264@institutmontilivi.cat>
2021-11-29 16:45:38 +0100

    versio 3 la serp es mou (no) good one

--- Snake2/MainWindow.xaml.cs
+++ Snake2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Snake2
 {
@@ -20,9 +21,36 @@ namespace Snake2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Snek jocSnek;
+        DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
+            jocSnek = new Snek();
+           
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
             InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            int y = (int)(cnCanvas.ActualHeight / Snek.Y_SIZE), x = (int)cnCanvas.ActualWidth / Snek.X_SIZE;
+            Ellipse ellSerp = new Ellipse()
+            {
+                Fill = Brushes.IndianRed,
+                Width = x,
+                Height = y
+            };
+            cnCanvas.Children.Add(ellSerp);
+            Canvas.SetTop(ellSerp, jocSnek.CapSerp.Y * y);
+            Canvas.SetLeft(ellSerp, jocSnek.CapSerp.X * x);
+        }
+
+        private void cnCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            jocSnek.Moure();
         }
     }
 }
